9061b3a95c2bd2501a188abb7421ee0fe9fef3cb
HyunSeoChoi <dev.hyunseo@gmail.com>
2022-01-24 21:55:33 +0900

    Button, ScrollView, TextInput 심화

--- react_native_01/App.js
+++ react_native_01/App.js
@@ -1,31 +1,58 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet, ScrollView} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Button,
+  TextInput,
+} from 'react-native';
 import Header from './src/header';
 import Generator from './src/generator';
 import NumList from './src/numlist';
 import Input from './src/input';
 
 class App extends Component {
+  // state = {
+  //   appName: 'Example App',
+  //   random: [],
+  // };
+
+  // onAddRandomNum = () => {
+  //   const randomNum = Math.floor(Math.random() * 100) + 1;
+  //   this.setState(prevState => {
+  //     return {
+  //       random: [...prevState.random, randomNum],
+  //     };
+  //   });
+  // };
+
+  // onNumDelete = position => {
+  //   const newArray = this.state.random.filter((num, index) => {
+  //     return position != index;
+  //   });
+  //   this.setState({
+  //     random: newArray,
+  //   });
+  // };
+
   state = {
-    appName: 'Example App',
-    random: [],
+    myTextInput: '',
+    alphabet: ['a', 'b', 'c', 'd'],
   };
 
-  onAddRandomNum = () => {
-    const randomNum = Math.floor(Math.random() * 100) + 1;
+  onChangeInput = event => {
+    this.setState({
+      myTextInput: event,
+    });
+  };
+
+  onAddTextInput = () => {
     this.setState(prevState => {
       return {
-        random: [...prevState.random, randomNum],
+        myTextInput: '',
+        alphabet: [...prevState.alphabet, prevState.myTextInput],
       };
-    });
-  };
-
-  onNumDelete = position => {
-    const newArray = this.state.random.filter((num, index) => {
-      return position != index;
-    });
-    this.setState({
-      random: newArray,
     });
   };
 
@@ -39,9 +66,29 @@ class App extends Component {
 
         <ScrollView style={{width: '100%'}} bounces={true}>
           <NumList num={this.state.random} delete={this.onNumDelete} />
-        </ScrollView> */}
-
+        </ScrollView> 
+        
         <Input />
+        */}
+        <Picker />
+        <TextInput
+          style={styles.input}
+          value={this.state.myTextInput}
+          onChangeText={this.onChangeInput}
+          multiline={true}
+          maxLength={100}
+          autoCapitalize={'none'}
+          editable={true}
+        />
+        <Button title="Add Text Input" onPress={this.onAddTextInput} />
+
+        <ScrollView style={{width: '100%'}}>
+          {this.state.alphabet.map((item, idx) => (
+            <Text style={styles.mainText} key={idx}>
+              {item}
+            </Text>
+          ))}
+        </ScrollView>
       </View>
     );
   }
@@ -72,6 +119,15 @@ const styles = StyleSheet.create({
     fontWeight: 'normal',
     color: 'red',
     padding: 20,
+    margin: 20,
+    backgroundColor: 'pink',
+  },
+  input: {
+    width: '100%',
+    backgroundColor: '#cecece',
+    marginTop: 20,
+    fontSize: 25,
+    padding: 10,
   },
 });
 
