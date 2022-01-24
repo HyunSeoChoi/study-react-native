1360520e576cc16aa544a014fc8be97183777a1d
HyunSeoChoi <dev.hyunseo@gmail.com>
2022-01-24 15:13:32 +0900

    Button 파트

--- react_native_01/App.js
+++ react_native_01/App.js
@@ -1,16 +1,27 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import Header from './src/header';
+import Generator from './src/generator';
+import NumList from './src/numlist';
 
 class App extends Component {
   state = {
     appName: 'Example App',
+    random: [36, 999],
+  };
+
+  onAddRandomNum = () => {
+    alert('Add random number!');
   };
 
   render() {
     return (
       <View style={styles.mainView}>
         <Header name={this.state.appName}></Header>
+        <Text style={styles.mainText}>Hello, World!</Text>
+        <Generator add={this.onAddRandomNum} />
+
+        <NumList num={this.state.random} />
       </View>
     );
   }
@@ -22,7 +33,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     paddingTop: 50,
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
   subView: {
     backgroundColor: 'yellow',
---
+++ react_native_01/src/numlist.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, Button} from 'react-native';
+
+const NumList = props => {
+  return props.num.map((item, idx) => (
+    <View style={styles.numList} key={idx}>
+      <Text>{item}</Text>
+    </View>
+  ));
+};
+
+const styles = StyleSheet.create({
+  numList: {
+    backgroundColor: '#CECECE',
+    alignItems: 'center',
+    padding: 5,
+    width: '100%',
+    marginTop: 5,
+  },
+});
+
+export default NumList;
