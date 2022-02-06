99464ec1b61370476fa29f9dfebebdb29377cf80
HyunSeoChoi <dev.hyunseo@gmail.com>
2022-02-06 18:10:26 +0900

    ActivityIndicator 파트

--- react_native_basic/src/picker.js
+++ react_native_basic/src/picker.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, ActivityIndicator} from 'react-native';
 import {Picker} from '@react-native-community/picker';
 import Slider from '@react-native-community/slider';
 
@@ -29,6 +29,12 @@ class PickerComponent extends Component {
           step={1}
         />
         <Text style={styles.input}>{this.state.value}</Text>
+        <ActivityIndicator
+          style={{paddingTop: 200}}
+          size="large"
+          color="green"
+          animating={true}
+        />
         <Picker
           style={{height: 50, width: 250}}
           selectedValue={this.state.country}
