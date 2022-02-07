0f40d186992a5f18711266c78885dd9358222216
HyunSeoChoi <dev.hyunseo@gmail.com>
2022-02-07 21:59:16 +0900

    StackScreen 파트

---
+++ react_navigation_basic/src/user.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+class UserScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>User Screen</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
+
+export default UserScreen;
