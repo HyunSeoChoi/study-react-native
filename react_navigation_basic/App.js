fee553aabb0379e1c357bce970282ed54a10c960
HyunSeoChoi <dev.hyunseo@gmail.com>
2022-02-07 23:17:06 +0900

    Header Bar 파트

--- react_navigation_basic/App.js
+++ react_navigation_basic/App.js
@@ -11,8 +11,12 @@ class App extends Component {
   render() {
     return (
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="User">
-          <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{title: 'Home Screen'}}
+          />
           <Stack.Screen
             name="User"
             component={UserScreen}
@@ -21,6 +25,17 @@ class App extends Component {
               userName: 'Gildong',
               userLastName: 'Go',
             }}
+            // options={{
+            //   title: 'User Screen',
+            //   headerStyle: {
+            //     backgroundColor: 'pink',
+            //   },
+            //   headerTintColor: 'red',
+            //   headerTitleStyle: {
+            //     fontWeight: 'bold',
+            //     color: 'purple',
+            //   },
+            // }}
           />
         </Stack.Navigator>
       </NavigationContainer>
