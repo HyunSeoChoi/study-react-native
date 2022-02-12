a36848f7d01ead4afaddef27680f768a6ae97f35
HyunSeoChoi <dev.hyunseo@gmail.com>
2022-02-12 18:58:11 +0900

    Image Picker 프로젝트명 변경

--- react_native_utility/android/app/src/main/java/com/react_native_imagepicker/MainActivity.java
+++ react_native_utility/android/app/src/main/java/com/react_native_imagepicker/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "react_native_imagepicker";
+    return "react";
   }
 }
