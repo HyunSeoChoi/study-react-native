ac206cc3d1805a3ceb8c2ce7f70eb0302d61abe7
HyunSeoChoi <dev.hyunseo@gmail.com>
2022-02-12 13:57:17 +0900

    Contacts 권한 설정

--- react_native_imagepicker/App.js
+++ react_native_imagepicker/App.js
@@ -8,13 +8,38 @@
 
 import React, {Component} from 'react';
 import {StyleSheet, View, Text, Image, Button} from 'react-native';
-import ImagePicker from 'react-native-image-picker';
+import ImagePicker, {
+  LaunchCamera,
+  launchImageLibrary,
+} from 'react-native-image-picker';
+import Contacts from 'react-native-contacts';
 
 class App extends Component {
+  state = {
+    avatar: '',
+  };
+
+  addImage = () => {
+    launchImageLibrary(
+      {
+        title: 'Choose your photo',
+        takePhotoButtonTitle: 'Take a pretty one',
+        chooseFromLibraryButtonTitle: 'Select an old one',
+        cancelButtonTitle: 'Just go back',
+      },
+      response => {
+        this.setState({
+          avatar: response.uri,
+        });
+      },
+    );
+  };
+
   render() {
     return (
-      <View style={style.container}>
-        <Text>Hello</Text>
+      <View style={styles.container}>
+        <Image source={{uri: this.state.avatar}} style={styles.avatar} />
+        <Button title="Add an Image" onPress={() => this.addImage()} />
       </View>
     );
   }
@@ -25,6 +50,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     backgroundColor: '#e4ab26',
+  },
+  avatar: {
+    width: '100%',
+    height: 400,
   },
 });
 
