75d10cc9611603835bff8b8e2d05c5bd83183d2a
HyunSeoChoi <dev.hyunseo@gmail.com>
2022-02-14 15:59:33 +0900

    애니메이션 효과 순서 파트

--- react_native_animation/App.js
+++ react_native_animation/App.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 import AnimOne from './src/Animation01';
+import AnimTwo from './src/Animation02';
 
 class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <AnimOne />
+        <AnimTwo />
       </View>
     );
   }
@@ -16,6 +17,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    paddingTop: 50,
   },
 });
 
