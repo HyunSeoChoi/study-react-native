5c78c44cd6b7e783294160514be58f1e89eef65f
HyunSeoChoi <dev.hyunseo@gmail.com>
2022-02-14 23:25:07 +0900

    Platform별 코딩 파트

--- react_common_sense/App.js
+++ react_common_sense/App.js
@@ -1,14 +1,33 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {Platform, StyleSheet, View, Text} from 'react-native';
 import SuperText from './src/utils/superText';
 
 class App extends Component {
+  checkSupport() {
+    if (Platform.OS === 'ios') {
+      if (Platform.Version < 13.4) {
+        return false;
+      }
+    } else {
+      if (Platform.Version < 27) {
+        return false;
+      }
+    }
+    return true;
+  }
   render() {
     return (
       <View style={styles.container}>
-        <SuperText style={{backgroundColor: 'red'}}>
-          This is my template!
-        </SuperText>
+        {this.checkSupport() ? (
+          <SuperText style={styles.div}>
+            {/* This is my template! */}
+            {Platform.OS === 'ios'
+              ? 'This is iOS Phone'
+              : 'This is my Android Phone'}
+          </SuperText>
+        ) : (
+          <Text>Sorry, Your phone is not being supported by the app.</Text>
+        )}
       </View>
     );
   }
@@ -20,6 +39,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#bbb',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  div: {
+    ...Platform.select({
+      ios: {
+        backgroundColor: 'yellow',
+      },
+      android: {
+        backgroundColor: 'red',
+      },
+    }),
   },
 });
 
